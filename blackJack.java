6305a67803d004ecd792fb68d08aa57b7ac6a986
Chris <c5keith90@gmail.com>
2013-10-29 09:05:27 -0600

    doit

---
+++ blackJack.java
@@ -0,0 +1,21 @@
+class blackJack {
+	private int suit;
+	private int number;
+	public void Card(int number, int suit) {
+		setNumber(number);
+		this.Deck = Deck;
+	}
+	public int setNumber() {
+		this.number = number;
+	}
+	public int getNumber() {
+		return this.number;
+	}
+	public void setSuit() {
+		this.suit = suit;
+	}
+	public  void suit() {
+		return this.suit;
+	}
+
+}
