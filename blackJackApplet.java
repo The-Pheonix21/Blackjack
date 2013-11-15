4b6586362d7d95637f1f08b02539288aeed48c78
Chris <c5keith90@gmail.com>
2013-11-15 08:29:31 -0700

    ok test

--- blackJackApplet.java
+++ blackJackApplet.java
@@ -1,48 +1,45 @@
 import java.awt.*;
 import java.applet.*;
-public class blackJackApplet extends Applet{
+public class blackJackApplet extends Applet {
 	private cardImage [] picturesAreHard;
 	private cardImage [] picturesAreHard1;
 	private Deck deck;
-	private int width = 50;
-	private int height = 70;
+	private int width = 80;
+	private int height = 100;
 
 	public void init() {
-		this.picturesAreHard = new cardImage[52];
-		this.picturesAreHard1 = new cardImage[52];
+		this.picturesAreHard = new cardImage[2];
+		this.picturesAreHard1 = new cardImage[2];
 		Card temp;
 		deck = new Deck();
-		// deck.shuffle();
-		for (int i = 0;i < 52; i++) {
+		 deck.shuffle();
+		for (int i = 0;i < 2; i++) {
 			temp = deck.dealCard();
 			picturesAreHard[i] = new cardImage(temp.getCardImage());
 
 		}
 		deck.shuffle();
-		for (int i = 0;i < 52; i++) {
-			temp = deck.dealCard();
-			picturesAreHard1[i] = new cardImage(temp.getCardImage());
+		 for (int j = 0;j < 2; j++) {
+		 	temp = deck.dealCard();
+		 	picturesAreHard1[j] = new cardImage(temp.getCardImage());
 
-		}
+		 }
 	}
 	public void paint(Graphics g) {
 		int countrow=0;
-		int x=50;
-		int y=50;
-		for (int i = 0;i < 52;i++) {
+		int x=100;
+		int y=100;
+		for (int i = 0;i < 2;i++) {
 			if ((i+1)%13==0) {
 				x=75+(width*(i%13)+(25*(i%13)));
 			}else {
 				x=75+(width*((i)%13))+(25*((i)%13));
 			}
-			if ((i%13==0) && (i>9)) {
-				countrow++;
-			}
 			y=50+(countrow*height);
 			picturesAreHard[i].draw(g,new Rectangle(x,y,width,height));
 		}
 		countrow=countrow+2;
-		for (int i = 0;i < 52;i++) {
+		for (int i = 0;i < 2;i++) {
 			if ((i+1)%13==0) {
 				x=75+(width*(i%13)+(25*(i%13)));
 			}else {
--- blackJackHand.java
+++ blackJackHand.java
@@ -1,3 +1,58 @@
-public class blackJackHand {
-	
+public class BlackjackHand {
+  int totalHandValue;
+  int cardCount;
+   boolean ace = false;
+   boolean aceHigh = false;
+   public BlackjackHand() {
+           // Create a Hand object that is initially empty.
+      totalHandValue=0;
+      cardCount=0;
+
+   }
+   
+   public void handClear() {
+         // Discard all the cards from the hand.
+      totalHandValue=0;
+      cardCount=0;
+   }
+   
+   
+   public int getCardCount() {
+         // Return the number of cards in the hand.
+      return cardCount;
+   }
+   
+   
+   public int BlackjackValue(Card cdealt) {
+            // Returns the value of this hand for the
+            // game of Blackjack
+      int cardVal;
+        cardCount++;
+       
+             cardVal = cdealt.getValue();  // The normal value, 1 to 13.
+             if (cardVal > 10) {
+                 cardVal = 10;   // For a Jack, Queen, or King.
+             }
+             if (cardVal == 1) {
+                 ace = true;     // There is at least one ace.
+             }
+             totalHandValue = totalHandValue + cardVal;
+         
+
+             // Now, val is the value of the hand, counting any ace as 1.
+             // If there is an ace, and if changing its value from 1 to 
+             // 11 would leave the score less than or equal to 21,
+             // then do so by adding the extra 10 points to val. 
+
+          if ( ace == true  &&  totalHandValue + 10 <= 21 ){
+              totalHandValue = totalHandValue + 10;
+             aceHigh=true;}
+             if (totalHandValue > 21 && aceHigh){
+              totalHandValue=totalHandValue - 10;
+              aceHigh=false;
+             }
+          return totalHandValue;
+
+     }  // end getBlackjackValue()
+  
 }
